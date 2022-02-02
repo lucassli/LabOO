14f9ee8ddc33f57271c893609a7ef35a0461dd94
Abel Lucas <abellucasfonseca@gmail.com>
2022-02-02 17:35:54 -0300

    Criação da Classe Pagamento. (Abel)

--- AutoEscolaTrevo/LabOOAutoEscola/Models/Pagamento.cs
+++ AutoEscolaTrevo/LabOOAutoEscola/Models/Pagamento.cs
@@ -2,5 +2,11 @@
 {
     public class Pagamento
     {
+        DateTime dataPagamento;
+        Boolean status;
+        
+        public DateTime DateTime { get; set; }
+        public Boolean Status { get; set; }
+
     }
 }
