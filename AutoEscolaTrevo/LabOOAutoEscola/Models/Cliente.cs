0eb03cbd1e40f72aebe1d32d333cdef6ec2b3b35
Lucas Lima <77409981+lucassli@users.noreply.github.com>
2022-02-01 21:15:56 -0300

    Padrao MCV criado e classes base

---
+++ AutoEscolaTrevo/LabOOAutoEscola/Models/Cliente.cs
@@ -0,0 +1,17 @@
+﻿namespace LabOOAutoEscola.Models
+{
+    public class Cliente
+    {
+        string nome;
+        DateTime dataNascimento;
+        int numeroIdentididade;
+        int cpf;
+        Boolean status;
+
+        public Boolean Status { get; set; }
+        public int Cpf { get; set; }
+        public int NumeroIdentidade { get; set; }
+        public string Nome { get; set; }
+        public string DataNascimento { get; set; }
+    }
+}
---
+++ AutoEscolaTrevo/LabOOAutoEscola/Models/Venda.cs
@@ -0,0 +1,6 @@
+﻿namespace LabOOAutoEscola.Models
+{
+    public class Venda
+    {
+    }
+}
