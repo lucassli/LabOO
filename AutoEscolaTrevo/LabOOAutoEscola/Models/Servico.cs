87b8bffe318646ae1651fe6f6a3c8577936b81d2
Gabriel Rodrigues <gabrielsantosrodrigues2000@gmail.com>
2022-02-04 18:35:17 -0300

    Adição das classes Servico e ItemServico

--- AutoEscolaTrevo/LabOOAutoEscola/Models/Servico.cs
+++ AutoEscolaTrevo/LabOOAutoEscola/Models/Servico.cs
@@ -2,5 +2,10 @@
 {
     public class Servico
     {
+        private string _nomeServico { get; set; }
+        private double _valorServico { get; set; }
+        private double _valorMinimo { get; set; }
+        private int _codigoServico { get; set; }
+        private double _taxaServico { get; set; }
     }
 }
