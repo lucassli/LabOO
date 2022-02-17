cbebec1b2a069ddc1701f57387e69a4be0c5354f
Lucas Lima <77409981+lucassli@users.noreply.github.com>
2022-02-17 20:20:04 -0300

    Novo Padrao Ok

--- AutoEscolaTrevoPOO/Models/PagamentoCarne.cs
+++ AutoEscolaTrevoPOO/Models/PagamentoCarne.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Globalization;
 using System.Threading;
 
@@ -6,13 +6,13 @@ namespace AutoEscolaTrevoPOO.Models
 {
     public class PagamentoCarne : Pagamento
     {
-        double valorPago;
-        double multaAtraso;
+        public double valorPago { get;}
+        public double multaAtraso { get;}
 
-        public PagamentoCarne()
+        public PagamentoCarne(double valorPago, double multaAtraso )
         {
-            private double _multaAtraso { get; set; }
-            private double _valorPago { get; set; }
+            this.valorPago = valorPago;
+            this.multaAtraso = multaAtraso;
         }
     }
 }
